13be1299a4ddac65a112fc536fac0530b9ee232d
Jinwoo Park <pjwapollon@gmail.com>
2023-02-09 15:40:58 +0900

    1.5차 완성본-detail버튼삽입 bug1 : detail버튼 누르면 페이지이동 bug2 : 계속 쌓임 포스터가 ㅠ

--- projects/indimovie/app.py
+++ projects/indimovie/app.py
@@ -15,14 +15,15 @@ def home():
 @app.route("/movie", methods=["POST"])
 def movie_post():
     url_receive = request.form['url_give']
-    # star_receive = request.form['star_give']
-    # comment_receive = request.form['comment_give']
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
+
+    #db.movies.remove({})
+    #db.movies.deleteMany({})
 
     movies = soup.select('body > div.content > div > div.movie-list.slider-type1 > ul > li')
     top10_count = 1
@@ -55,6 +56,24 @@ def movie_post():
 def movie_get():
     movie_list = list(db.movies.find({}, {'_id':False}))
     return jsonify({'movies':movie_list})
+
+@app.route("/movie", methods=["GET"])
+def detail(id):
+    # DB 조회하도록 하는 로직 작성
+    # 예시: movie = db.collection.find_one({"_id": ObjectId(id)})
+    # DB 조회 결과를 detail.html에 전달
+    movie = db.collection.find_one({"_id": ObjectId(id)})
+
+    return render_template("detail.html", movie=movie)
+
+@app.route("/")
+def index(id):
+    # DB 조회하도록 하는 로직 작성
+    # 예시: movies = db.collection.find({})
+    # DB 조회 결과를 detail.html에 전달
+    movies = db.collection.find({})
+    return render_template("index.html", movies=movies)
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
